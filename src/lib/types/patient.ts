7768ba755b6a9b2fe09939dc3c0d916064b13e3d
igornevesg <igornevesmoc@gmail.com>
2026-06-29 19:59:31 -0300

    remoção campo RG

--- src/lib/types/patient.ts
+++ src/lib/types/patient.ts
@@ -1,2 +1,2 @@
-export type PatientData = { nome:string; cpf:string; dataNasc:string; cargo:string; empregador:string; rg:string; cnpj:string; dataExame:string; examesComplementares:string; raioXSolicitado:string; };
-export const initialPatientData: PatientData = { nome:"", cpf:"", dataNasc:"", cargo:"", empregador:"", rg:"", cnpj:"", dataExame:"", examesComplementares:"", raioXSolicitado:"" };
+export type PatientData = { nome:string; cpf:string; dataNasc:string; cargo:string; empregador:string; cnpj:string; dataExame:string; examesComplementares:string; raioXSolicitado:string; };
+export const initialPatientData: PatientData = { nome:"", cpf:"", dataNasc:"", cargo:"", empregador:"", cnpj:"", dataExame:"", examesComplementares:"", raioXSolicitado:"" };
